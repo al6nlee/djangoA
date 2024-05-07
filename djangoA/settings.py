36582c27d83d90951298c6cb6d7b31acc8d653d4
alan <al6nlee@gmail.com>
2024-05-08 00:53:16 +0800

    init3

--- djangoA/settings.py
+++ djangoA/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangoA.apps.user',
+    'rest_framework',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -78,9 +79,14 @@ WSGI_APPLICATION = 'djangoA.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':
+    {
+        'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
+        'NAME': 'djangoA', # 数据库名称
+        'HOST': '172.26.22.201', # 数据库地址
+        'PORT': 3306, # 端口
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': '12345678', # 数据库密码
     }
 }
 
